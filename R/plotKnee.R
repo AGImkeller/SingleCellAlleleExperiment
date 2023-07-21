b28cdfbfae9a8e6120e3f2207e520ed3b0208719
Jonas <Jonas.Schuck@kgu.de>
2023-07-21 14:32:11 +0200

    adding values to rowData$Symbol for the allele_id rows - updating the biomaRt function and adding NA values to rows where there is no ncbi_symbol name

---
+++ R/plotKnee.R
@@ -0,0 +1,55 @@
+################################################################################
+############---function plots a knee plot ranking barcode counts---#############
+################---majority of code taken from Ahmad Al Ajami---################
+################################################################################
+
+
+#------------------------------Knee plot---------------------------------------#
+
+#####
+
+#' Knee plot
+#'
+#' @description
+#' Creates a Kneeplot ranking the barcodes according to their total UMI count. The plot is
+#' used to determine a threshold for filtering barcodes in the preprocessing step.
+#'
+#'
+#' @param path file path of the directory containing the input files as character string
+#'
+#' @import dplyr
+#' @import tibble
+#' @import ggplot2
+#'
+#' @return returns a knee plot for determining a count threshold used for filtering out barcodes
+#' @export
+plotKnee <- function(path){
+  barcodes_loc <- file.path(path, "barcodes.txt")
+  features_loc <- file.path(path, "features.txt")
+  matrix_loc   <- file.path(path, "matrix.mtx")
+
+  barcodes <- read.csv(barcodes_loc, sep = "", header = FALSE)
+  features <- read.delim(features_loc, header = FALSE)
+  matrix   <- readMM(matrix_loc)
+
+  rownames(matrix) <- paste(barcodes$V1, "_D1", sep = "")
+  colnames(matrix) <- features$V1
+  colnames(matrix) <- gsub("\\|.*","", colnames(matrix))
+  colnames(matrix) <- gsub("\\-",".", colnames(matrix))
+
+  tot_counts <- Matrix::rowSums(matrix)
+
+  df <- tibble(cells = rownames(matrix),
+               total = tot_counts,
+               rank = row_number(dplyr::desc(total))) %>%
+    distinct() %>%
+    dplyr::arrange(rank)
+
+  total <- df$total
+
+  ggplot2::ggplot(df, aes(total, rank)) +
+    geom_path() +
+    scale_x_log10() + scale_y_log10() + annotation_logticks() +
+    labs(y = "Barcode rank", x = "Total UMI count")
+}
+#####
